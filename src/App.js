73b76a66927bd1523083251949c6eff85f678677
Bob Saris <bob@bobsaris.com>
2018-09-02 10:53:28 -0400

    Updated config to allow for scss and added some classes to make sure it works.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="ui-app">
-        <h1>Hello World!</h1>
+        <h1 className="ui-app__header">Hello World!</h1>
       </div>
     );
   }
